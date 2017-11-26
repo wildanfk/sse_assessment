7971fb338f80c049ea855a98c55392e96d5d5b10
Wildan Febria Khasani <wildan.fk@gmail.com>
2017-11-26 21:04:49 +0700

    formula score

--- sse_a1.py
+++ sse_a1.py
@@ -2,8 +2,20 @@
 path_user = '/Users/wildanfk/Documents/other_workspace/sse/sse_data_sample/samples/user.tsv'
 path_product = "/Users/wildanfk/Documents/other_workspace/sse/sse_data_sample/samples/product.tsv"
 
+# File Product
+try:
+    file_product = open(path_product, 'r')
+except:
+    print("No such file : '%s'" %(path_product))
+    exit()
+# File User
+try:
+    file_user = open(path_user, 'r')
+except:
+    print("No such file : '%s'" %(path_product))
+    exit()
+
 # Store the product score into memory
-file_product = open(path_product, 'r')
 data_product = None
 try:
     data_product = list(map(
@@ -20,7 +32,19 @@ file_product.close()
 if(data_product is None):
     exit()
 
-for d in data_product:
-    print(d)
+
+# Formula Score
+from datetime import datetime, timezone
+func_sc_user_pref = lambda sc_user_product, timestamps : sc_user_product * pow(0.95, (datetime.now(timezone.utc) - datetime.fromtimestamp(timestamps, timezone.utc)).days)
+func_sc_rec_product = lambda sc_user_pref, sc_product : sc_user_pref + sc_product
 
 
+# Process the user data
+error_process = 0
+for fu in file_user:
+    u = fu.strip().split('\t')
+    print(u)
+
+
+
+file_user.close()
