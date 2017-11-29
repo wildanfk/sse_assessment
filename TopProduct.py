45384a33d10e19f3ffb9d0c8a2f5730da7c6449e
Wildan Febria Khasani <wildan.fk@gmail.com>
2017-11-29 22:18:43 +0700

    json able top product

--- TopProduct.py
+++ TopProduct.py
@@ -1,17 +1,15 @@
-import operator
-
 class TopProduct():
     def __init__(self):
         self.top = {} # pid : (score, status_default)
 
     def add(self, pid, score, default = False):
         if ((pid not in self.top) or
-            (pid in self.top and self.top[pid][0] < score) or
-            (pid in self.top and self.top[pid][1] == True)):
-            self.top[pid] = (score, default)
+            (pid in self.top and self.top[pid]['s'] < score) or
+            (pid in self.top and self.top[pid]['d'] == True)):
+            self.top[pid] = {'s' : score, 'd' : default}
 
     def get(self):
-        return sorted(self.top.items(), key=operator.itemgetter(1), reverse=True)
+        return sorted(self.top.items(), key=lambda x : x[1]['s'], reverse=True)
 
     def getTop(self, top_n = 5):
         return self.get()[0:top_n]
